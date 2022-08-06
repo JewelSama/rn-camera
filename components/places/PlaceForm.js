c9dbe7cc78ed4b672d9b70d4bd25b0a1f0371324
Jewel Osafile <osafilejewel@gmail.com>
2022-08-06 13:10:11 +0100

    navigation and screens

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { Colors } from '../../constants/colors'
 
 const PlaceForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+
+    const ChangeTitleHandler = (enteredText) => {
+        setEnteredTitle(enteredText)
+    }
+
+
   return (
     <View>
-      <Text>The Place form</Text>
+      <ScrollView style={styles.form}>
+        <View>
+            <Text style={styles.label}>Title</Text>
+            <TextInput  style={styles.input} onChangeText={ChangeTitleHandler} value={enteredTitle} />
+        </View>  
+      </ScrollView> 
     </View>
   )
 }
 
 export default PlaceForm
+
+const styles = StyleSheet.create({
+    form: {
+        padding: 24
+    },
+    label: {
+        fontWeight: "bold",
+        marginBottom: 4,
+        color: Colors.primary500
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100
+    }
+})
--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,29 +1,57 @@
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import PlaceItem from './PlaceItem'
+import { Colors } from '../../constants/colors'
 
 const PlacesList = ({places}) => {
-    if(!places || places.length === 0){
-        return <View style={styles.fallBackContainer}>
-            <Text style={styles.fallBackText}>No Places Added Yet- add some</Text>
-        </View>
-    }
-  return 
-  <FlatList data={places} 
-    keyExtractor={(item) => item.id}
-    renderItem={({item}) => <PlaceItem place={item} />}
-   />
+    
+// if(!places) {
+//     return (<View style={styles.fallBackContainer}>
+//                  <Text style={styles.fallBackText}>No Places Added Yet- add some</Text>
+//              </View>
+//     )
+
+// }
+return(
+<View>
+    {!places || places.length=== 0 || places === "" ? (
+        <View style={styles.fallBackContainer}>
+                          <Text style={styles.fallBackText}>No Places Added Yet- add some</Text>
+                      </View>
+    ) : (
+        <FlatList data={places} 
+         keyExtractor={(item) => item.id}
+         renderItem={({item}) => <PlaceItem place={item} />}
+         />
+    )}
+</View>
+)
+
+    
+    // if(!places || places.length === 0){
+    //     return (<View style={styles.fallBackContainer}>
+    //         <Text style={styles.fallBackText}>No Places Added Yet- add some</Text>
+    //     </View>
+    //     )
+    // } else {
+    //     return (
+    //     <FlatList data={places} 
+    //     keyExtractor={(item) => item.id}
+    //     renderItem={({item}) => <PlaceItem place={item} />}
+    //     />
+    //     )
+    // }
 }
 
 export default PlacesList
 
 const styles = StyleSheet.create({
     fallBackContainer: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
     fallBackText: {
-        fontSize: 16
+        fontSize: 16,
+        color: Colors.primary200
     }
 })
