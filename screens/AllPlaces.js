50c382d289dbe87d304a07be45580616ad3e1bbb
Jewel Osafile <osafilejewel@gmail.com>
2022-08-06 11:32:41 +0100

    screens and components

---
+++ screens/AllPlaces.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import PlacesList from '../components/places/PlacesList'
+
+const AllPlaces = () => {
+  return (
+    <View>
+      <PlacesList />
+    </View>
+  )
+}
+
+export default AllPlaces
